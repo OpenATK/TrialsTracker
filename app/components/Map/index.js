5f24f4a38e8c8dc791a09df7dd58dc21303e8153
Sam Noel <noel.samuel.a@gmail.com>
2016-04-22 17:03:26 -0400

    store temp work

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -1,89 +1,145 @@
 import React, { Proptypes } from 'react';
 import { Decorator as Cerebral, Link } from 'cerebral-view-react';
-import { Map, TileLayer, ImageOverlay } from 'react-leaflet';
+import { Polygon, Marker, Map, TileLayer, ImageOverlay, latLng, latLngBounds} from 'react-leaflet';
 import styles from './map.css';
 import uuid from 'uuid';
-
+import YieldTileLayer from '../CustomTileLayer/';
+var LatLon = require('geodesy').LatLonEllipsoidal;
 var GeoJSON = require('react-leaflet').GeoJson;
+//import jsonData from './2015J4.js';
+//import jsonDataPoly from './2015J4Poly.js';
+import converter from 'hsl-to-rgb';
+import Color from 'color';
+import geojsonvt from 'geojson-vt';
+//import { makeGeoJsonPolygons, makeGeoJsonPoints } from './geotiffConverter.js';
+import { buildImage } from './geotiffConverter.js';
+//import { create } from 'lwip';
+//var c = require('lwip').create;
+import ph from './ph_4326.js';
+var tileIndex;
 
 @Cerebral((props) => {
-	return{
-		notes: ['home', 'model', 'notes'],
-		selectedNote: ['home', 'model', 'selected_note'],
-		//note: ['home', 'model', 'notes', props.id],
-	};
+  return{
+    notes: ['home', 'model', 'notes'],
+    selectedNote: ['home', 'model', 'selected_note'],
+    //note: ['home', 'model', 'notes', props.id],
+  };
 })
 
 class _Map extends React.Component {
 
-  static propTypes = {
+  componentWillMount() {
+    console.log('component will mount');
     
-  };
-  
+    let tileOptions = {
+      maxZoom:20
+    };
+ //   tileIndex = geojsonvt(ph, tileOptions);
+  }
+
+/* 
+  getColor(val, min, max) {
+    var rng = (max-min);
+    var h = (val - min)/(rng)*150;
+    var s = 100;
+    var l = 50;
+    return Color().hsl(h,s,l).rgbString();
+  }
+
+  createPoly(feature1, feature2) {
+    // create a LatLon from the point
+    var pt1 = LatLon(feature1.geometry.coordinates[1], feature1.geometry.coordinates[0]);
+    var pt2 = LatLon(feature2.geometry.coordinates[1], feature2.geometry.coordinates[0]);
+
+    //find the angle between this point and the previous
+    var angle1 = feature1.properties.Track_deg_;
+    var angle2 = feature2.properties.Track_deg_;
+
+    //build the polygon positions array by taking the tangent
+    var vertexA = pt1.destinationPoint(2.286, angle1+90);
+    var vertexB = pt1.destinationPoint(2.286, angle1-90);
+    var vertexC = pt2.destinationPoint(2.286, angle2+90);
+    var vertexD = pt2.destinationPoint(2.286, angle2-90);
+    var positions = [[
+      [vertexA.lon, vertexA.lat],
+      [vertexB.lon, vertexB.lat],
+      [vertexD.lon, vertexD.lat],
+      [vertexC.lon, vertexC.lat],
+      [vertexA.lon, vertexA.lat]
+    ]];
+    feature1.geometry.type = "Polygon";
+    feature1.geometry.coordinates = positions;
+    return feature1;
+  }
+
+  getPolygonVertices(feature1, feature2) {
+    // create a LatLon from the point
+    var pt1 = LatLon(feature1.geometry.coordinates[1], feature1.geometry.coordinates[0]);
+    var pt2 = LatLon(feature2.geometry.coordinates[1], feature2.geometry.coordinates[0]);
+
+    //find the angle between this point and the previous
+    var angle1 = feature1.properties.Track_deg_;
+    var angle2 = feature2.properties.Track_deg_;
+
+    //build the polygon positions array by taking the tangent
+    var vertexA = pt1.destinationPoint(2.286, angle1+90);
+    var vertexB = pt1.destinationPoint(2.286, angle1-90);
+    var vertexC = pt2.destinationPoint(2.286, angle2+90);
+    var vertexD = pt2.destinationPoint(2.286, angle2-90);
+    var positions = [
+      [vertexA.lat, vertexA.lon],
+      [vertexB.lat, vertexB.lon],
+      [vertexD.lat, vertexD.lon],
+      [vertexC.lat, vertexC.lon]
+    ];
+    return positions;
+  } 
+*/
   render() {
-    var position = [40.50000, -87.666666];
+  //  var position = [48.99, -104.05];
+  //  var position = [jsonData.features[0].geometry.coordinates[1], jsonData.features[0].geometry.coordinates[0]];
     var geoJSONData = [];
     _.each(this.props.notes, function(note) {
       geoJSONData.push(<GeoJSON data={note.geojson} color={note.color} key={uuid.v4()}/>);
     });
-    
-    var testData = {
-      "dp1wmhg1d": {
-        lat: 40.500000,
-        lon: -87.666666,
-        val: 15
-      },
-      "dp1wmhg19": {
-        lat: 40.500004,
-        lon: -87.666714,
-        val: 12
-      },
-      "dp1wmhg13": {
-        lat: 40.499961,
-        lon: -87.666714,
-        val: 17
-      },
-      "dp1wmhg16": {
-        lat: 40.499961,
-        lon: -87.666671,
-        val: 18
-      }
-    };
 
-    var geoj = {
-      "type":"FeatureCollection",
-      "features":[{
-        "type":"Feature",
-        "geometry": {
-          "type":"MultiPolygon",
-          "coordinates": [[[
-            [-87.666666, 40.500000], 
-            [-87.666714, 40.500004], 
-            [-87.666714, 40.499961], 
-            [-87.666671, 40.499961]
-          ]]]
-        },
-      }]
+/*
+    var pollies = [];
+    var positions;
+    for (let i = jsonData.features.length-1; i > 1; i--) {
+      positions = this.getPolygonVertices(jsonData.features[i], jsonData.features[i-1]);
+      var col = this.getColor(jsonData.features[i].properties.Yld_Vol_We,min,max);
+      var poly = <Polygon positions={positions} key={uuid.v4()} stroke={false} fillColor={col} fillOpacity={1.0} />;
+      pollies.push(poly);
+      newJson.features[i] = this.createPoly(jsonData.features[i], jsonData.features[i-1]);
     }
-
-    var b = _(testData).filter('val').reduce(function(a,m,i,p) {
-      return a + m.val/p.length;
-    },0);
-
+    newJson.features.splice(0, 1);
+    var geoj = <GeoJSON data={newJson} key={uuid.v4()}/>;
+*/  
+    var position = [4.497, -72.842];
     const signals = this.props.signals.home;
+//            url='http://{s}.{base}.maps.cit.api.here.com/maptile/2.1/{type}/{mapID}/hybrid.day/{z}/{x}/{y}/{size}/{format}?app_id={app_id}&app_code={app_code}&lg={language}'
+//            attribution='Map &copy; 1987-2014 <a href="http://developer.here.com">HERE</a>'
 
+//            url="http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png"
+//            attribution='Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+
+//            url='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+//            attribution='Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+  //        <YieldTileLayer tileIndex={tileIndex}/>
+
+      //    <ImageOverlay 
+       //     url={buildImage(ph)}
+       //     bounds={bounds}
+       //   />
     return (
       <div id='map-panel'>
-        <Map onLeafletMousedown={(e) => {signals.mouseDownOnMap({vertex_value: e.latlng, select_note: this.props.selectedNote, newSelectedNote:this.props.id})}} dragging={false} center={position} zoom={13}>
+        <Map onLeafletMousedown={(e) => {signals.mouseDownOnMap({vertex_value: e.latlng, select_note: this.props.selectedNote, newSelectedNote:this.props.id})}} dragging={true} center={position} zoom={19} tms={true}>
           <TileLayer
             url="http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png"
             attribution='Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
           />
-          {geoJSONData}
-          <ImageOverlay
-            url={om}
-            bounds={[[4.418, -72.91],[4.5935, -72.7814]]}
-          />
+          <YieldTileLayer/>
         </Map> 
       </div>
     );
--- app/modules/Home/mapchain.js
+++ app/modules/Home/mapchain.js
@@ -9,36 +9,21 @@ export var mouseMoveOnmap = [
 
 
 function dropPointFunc ({input, state}) {
-	console.log('mapMouseDown drop a point');
-	console.log(input);
-	console.log(state.get());
 	
 	var vertex = [input.vertex_value.lng, input.vertex_value.lat];
 	var currentSelectedNoteId = input.select_note;
-	console.log(vertex);
-	console.log(currentSelectedNoteId);
 		
 	_.each(state.get(['home', 'model', 'notes']), function(note) {
-		console.log('note.id and selectedNote');
-		console.log(note.id);	
 		if(note.id === currentSelectedNoteId){
-			console.log(note.id);
-			console.log(state.get(['home', 'model', 'notes', note.id, 'geojson', 'features', '0', 'geometry', 'coordinates', '0', '0']));
-			console.log(vertex);
-			console.log('before push');
 
 			//state.select(['home', 'model', 'notes', note.id, 'geojson', 'features', '0', 'geometry', 'coordinates', '0', '0']).push(vertex);
 			state.push(['home', 'model', 'notes', note.id, 'geojson', 'features', '0', 'geometry', 'coordinates', '0', '0'], vertex);
-			console.log('after push');
-			console.log(state.get(['home', 'model', 'notes', note.id, 'geojson', 'features', '0', 'geometry', 'coordinates']));
 		}
 	});
 
 };
 
 function mapMouseMove ({input, state}) {
-	console.log('mapMouseMove signal');
-	console.log(input);
 																					 
 };
 
