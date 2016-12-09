86aaeb0586671b8ca17bdbf5d757f2c4458b148c
Cyrus Bowman <cyrusbow@gmail.com>
2016-12-09 13:24:42 -0500

    Removed unreferenced files. Added some requirements to package.json

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,5 @@
 import { Controller } from 'cerebral';
 import Model from 'cerebral/models/immutable'
-//import model from './modules/app/model';
 
 import App from './modules/app'
 import Devtools from 'cerebral-module-devtools'
