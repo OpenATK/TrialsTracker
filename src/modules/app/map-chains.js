01cca68cbd87b7541f66b86fce5571378cff435e
EC2 Default User <ec2-user@ip-172-31-40-212.ec2.internal>
2017-01-31 13:39:24 +0000

    hotfixes on demo

--- src/modules/app/map-chains.js
+++ src/modules/app/map-chains.js
@@ -128,8 +128,8 @@ function longestCommonPrefix(strings) {
 }
 
 function computeNoteStats({input, state, output}) {
-  var token = state.get('app.settings.data_sources.yield.oada.token');
-  var domain = state.get('app.settings.data_sources.yield.oada.domain');
+  var token = state.get('app.settings.data_sources.yield.oada_token');
+  var domain = state.get('app.settings.data_sources.yield.oada_domain');
   var availableGeohashes = state.get(['app', 'model', 'yield_data_index']);
   var baseUrl = 'https://' + domain + '/bookmarks/harvest/tiled-maps/dry-yield-map/crop-index/';
   var polygon = state.get(['app', 'model', 'notes', input.id, 'geometry', 'geojson', 'coordinates'])[0];
